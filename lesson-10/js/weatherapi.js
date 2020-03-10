3bebdda94955718f7332042bce88f3635f04bc00
JenikaH <59703597+JenikaH@users.noreply.github.com>
2020-03-10 12:21:33 -0600

    api

---
+++ lesson-10/js/weatherapi.js
@@ -0,0 +1,20 @@
+/* The querystring name/value pair settings in the URL should be:
+a city id for Preston, Idaho,
+temperature units in Fahrenheit (imperial), and
+your own APPID.*/
+
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=6828ab982799b5bafd721bd612a9faec';
+
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+
+        document.getElementById('current-temp').textContent = jsObject.main.temp;
+
+        const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png'; // note the concatenation
+        const desc = jsObject.weather[0].description; // note how we reference the weather array
+        document.getElementById('imagesrc').textContent = imagesrc; // informational specification only
+        document.getElementById('icon').setAttribute('src', imagesrc); // focus on the setAttribute() method
+        document.getElementById('icon').setAttribute('alt', desc);
+    });
